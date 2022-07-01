169c68964a8579c61a6ab891a9c5693e58434d0e
Okil <okilnote6pro@gmail.com>
2022-07-02 00:46:03 +0500

    Add base bot logic

---
+++ config.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = "5344397143:AAEP9ZllJFa6d1lFEVJY3OeMloXqWcECi5I"
+APP_URL = "https://testbotmrokil.herokuapp.com/" + BOT_TOKEN
---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import telebot
+import logging
+from config import *
+from flask import Flask, request
+
+bot = telebot.Telebot(BOT_TOKEN)
+server = Flask(__name__)
+logger = telebot.logger 
+logger.setLevel(logging.DEBUG)
+
+@bot.message_handler(command=["start"])
+def start(message):
+    username = message.from_user.username
+    bot.reply_to(message, f"Hello, {username}!")
+
+
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redicted_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Uptade.de_json(json_string)
+    bot.process_new_uptades([update])
+    return "!", 200
+
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
