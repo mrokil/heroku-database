11843e751900a1f7ffb2a5a57156aa4a3d4ee0b0
Okil <okilnote6pro@gmail.com>
2022-07-04 23:26:03 +0500

    Add database connection

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5344397143:AAEP9ZllJFa6d1lFEVJY3OeMloXqWcECi5I"
 APP_URL = "https://testbotmrokil.herokuapp.com/" + BOT_TOKEN
-DB_URI = "postgres://vezgugooalnyrs:4ba6f7c7939fd2725f99576e332584b2fd4425d53d4dfd42dbfe2f80b736446d@ec2-54-228-125-183.eu-west-1.compute.amazonaws.com:5432/ddhkea4tj0gohh"
+DB_URI = "postgres://spkdvthjhlvswt:8b863b49e11821cf8ff392d00df7027a19e34324f7f75c4f725fbdbd0225283b@ec2-34-242-84-130.eu-west-1.compute.amazonaws.com:5432/d8tunv8pags0j4"
